6d9eae527e1c1273d31e19300370f9bf933ea4f0
Franco Canzani <francocanzani@gmail.com>
2024-11-24 00:07:38 +0100

    fix config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,8 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  define: {
-    'process.env': process.env,
-    VITE_API_URL: process.env.VITE_API_URL,
-  },
 });
