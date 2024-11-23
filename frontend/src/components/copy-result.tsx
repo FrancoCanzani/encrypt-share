72f9fdd779b70d69bb882bd92e0d04804cfef670
Franco Canzani <francocanzani@gmail.com>
2024-11-23 16:33:17 +0100

    adds share buttons

--- frontend/src/components/copy-result.tsx
+++ frontend/src/components/copy-result.tsx
@@ -1,4 +1,10 @@
 import { CopyButton } from './copy-to-clipboard-button';
+import {
+  TelegramShareButton,
+  WhatsappShareButton,
+  WhatsappIcon,
+  TelegramIcon,
+} from 'react-share';
 
 export default function CopyResult({
   resultKey,
@@ -7,15 +13,32 @@ export default function CopyResult({
   resultKey: string;
   resultId: string;
 }) {
+  const url = `http://localhost:5173/${resultId}#${resultKey}`;
+
   return (
     <div className='min-h-6 p-2 mt-8 space-x-4 flex items-center justify-start border border-white'>
-      <span className='truncate group relative font-medium text-sm'>
-        http://localhost:5173/{resultId}#{resultKey}
-      </span>
-      <CopyButton
-        className='text-white bg-black border rounded-none border-gray-300 hover:bg-black hover:ring-2 hover:ring-gray-400 hover:border-transparent transition duration-300 ease-in-out'
-        textToCopy={`http://localhost:5173/${resultId}#${resultKey}`}
-      />
+      <span className='truncate group relative font-medium text-sm'>{url}</span>
+      <div className='flex items-center jus space-x-5'>
+        <CopyButton
+          className='text-white bg-black border rounded-none border-gray-300 hover:bg-black hover:ring-2 hover:ring-gray-400 hover:border-transparent transition duration-300 ease-in-out'
+          textToCopy={url}
+        />
+        <TelegramShareButton
+          url={url}
+          title={'Check this encrypted message.'}
+          className='Demo__some-network__share-button'
+        >
+          <TelegramIcon size={32} round />
+        </TelegramShareButton>
+        <WhatsappShareButton
+          url={url}
+          title={'Check this encrypted message.'}
+          separator=':: '
+          className='Demo__some-network__share-button'
+        >
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+      </div>
     </div>
   );
 }
