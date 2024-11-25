4b3ab71b9d5742e0432fe2f8167fa9ce26dfe86b
Franco Canzani <francocanzani@gmail.com>
2024-11-25 21:48:00 +0100

    adds rate limit

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/mattn/go-sqlite3 v1.14.24
+	golang.org/x/time v0.8.0
 )
 
 require (
--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,12 +1,62 @@
 package server
 
 import (
+	"sync"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/time/rate"
 )
+
+type IPRateLimiter struct {
+    ips map[string]*rate.Limiter
+    mu  *sync.RWMutex
+    r   rate.Limit
+    b   int
+}
+
+func NewIPRateLimiter(r rate.Limit, b int) *IPRateLimiter {
+    return &IPRateLimiter{
+        ips: make(map[string]*rate.Limiter),
+        mu:  &sync.RWMutex{},
+        r:   r,
+        b:   b,
+    }
+}
+
+func (i *IPRateLimiter) GetLimiter(ip string) *rate.Limiter {
+    i.mu.Lock()
+    defer i.mu.Unlock()
+
+    limiter, exists := i.ips[ip]
+    if !exists {
+        limiter = rate.NewLimiter(i.r, i.b)
+        i.ips[ip] = limiter
+    }
+
+    return limiter
+}
 
 func (s *Server) RegisterRoutes() *gin.Engine {
     r := gin.Default()
+
+    r.Use(gin.Recovery())
+    r.Use(gin.Logger())
+
+    limiter := NewIPRateLimiter(rate.Limit(60/60.0), 1)
+
+    // Rate limiter middleware
+    r.Use(func(c *gin.Context) {
+        limiter := limiter.GetLimiter(c.ClientIP())
+        if !limiter.Allow() {
+            c.JSON(429, gin.H{
+                "error": "Too many requests. Please try again later.",
+            })
+            c.Abort()
+            return
+        }
+        c.Next()
+    })
 
     r.Use(cors.New(cors.Config{
         AllowOrigins:     []string{"http://localhost:5173", "https://encrypt-share-rose.vercel.app"},
